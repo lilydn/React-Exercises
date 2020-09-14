f984eabbef7e327c27cb343b93eaf99e922cc2d1
lilydn <kaganli404@gmail.com>
2020-09-14 15:07:07 +0300

    moved the increment function to be outside the render method

--- 7.1-increment/IncrementBtn.js
+++ 7.1-increment/IncrementBtn.js
@@ -6,14 +6,15 @@ class IncrementBtn extends React.Component {
     super(props);
     this.state = { value: 0 };
   }
+
+  increment = () => {
+    this.setState({ value: this.state.value + 1});
+  }
+
   render () {
-    const increment = () => {
-      this.setState({ value: this.state.value + 1});
-      console.log(this.state.value);
-    }
     return (
       <div className="increment">
-        <button onClick={increment}>increment</button>
+        <button onClick={this.increment}>increment</button>
         <label className="value-label">{this.state.value}</label>
       </div>
     );
